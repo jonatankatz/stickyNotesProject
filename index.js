e88288f0739b4304e63d7b7a4418943aabe24600
jonatan katz <jonatankatz4@gmail.com>
2019-07-22 15:42:44 +0300

    i started the form

---
+++ index.js
@@ -0,0 +1,9 @@
+
+Date.prototype.toDateInputValue = (function() {
+    let local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+document.getElementById("time-to-do").value = new Date().toDateInputValue();
+
+
